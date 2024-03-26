d5d0f3972addb14da152d5fa410b887ff3b30180
Cláudia Mesquita <cmmesquita3104@gmail.com>
2024-03-26 18:17:39 +0000

    Working on homepage setup

--- marinechoice/lib/main.dart
+++ marinechoice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marinechoice/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
