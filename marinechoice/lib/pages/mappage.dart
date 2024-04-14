42df69a0f5a04d6de6fbdd69d6c16cd0120adb0e
Cláudia Mesquita <cmmesquita3104@gmail.com>
2024-04-14 19:03:20 +0100

    Added footer and search bar to map, made back button work

--- marinechoice/lib/pages/mappage.dart
+++ marinechoice/lib/pages/mappage.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+
+import 'homepage.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -22,22 +24,25 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
+      bottomNavigationBar: buildBottomNavigationBar(),
       body: _currentP == null
           ? const Center(
-              child: Text("Loading...", style: TextStyle(fontSize: 40),),
-            )
+        child: Text("Loading...", style: TextStyle(fontSize: 40),),
+      )
           : GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: _currentP!,
-                zoom: 3,
-              ),
-              markers: {
-                Marker(
-                    markerId: MarkerId("_currentLocation"),
-                    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-                    position: _currentP!),
-              },
-            ),
+        initialCameraPosition: CameraPosition(
+          target: _currentP!,
+          zoom: 3,
+        ),
+        markers: {
+          Marker(
+              markerId: MarkerId("_currentLocation"),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueBlue),
+              position: _currentP!),
+        },
+      ),
     );
   }
 
@@ -73,4 +78,117 @@ class _MapPageState extends State<MapPage> {
       }
     });
   }
+
+  _navigate(int index) {
+    switch (index) {
+      case 0:
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const HomePage()));
+        break;
+      case 3:
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const MapPage()));
+        break;
+    }
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      backgroundColor: const Color(0xff5B92C6),
+      currentIndex: 3,
+      onTap: _navigate,
+      selectedItemColor: Colors.white,
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/home.svg', height: 25, width: 30,),
+          label: ("HOME"),),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/fishing-rod.svg', height: 25, width: 30,),
+          label: ("FIND"),),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/chef-hat.svg', height: 30, width: 30,),
+            label: ("COOK")),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/map.svg', height: 30, width: 30,),
+            label: ("MAP")),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/envelope-open.svg', height: 25, width: 30,),
+            label: ("SHARE")),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/user.svg', height: 30, width: 30,),
+            label: ("YOU")),
+      ],
+      selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w900,),
+      unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w900,),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      flexibleSpace: Container(
+        margin: const EdgeInsets.only(left: 55, top: 30, right: 55, bottom: 5),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: const LinearGradient(
+              colors: [Color(0xffD6E7F7), Color(0xffD6E7F7)],
+            )),
+      ),
+      title: TextField(
+        style: const TextStyle(color: Colors.black),
+        cursorColor: Colors.black87,
+        decoration: InputDecoration(
+          suffixIcon: Padding(
+              padding:
+              const EdgeInsets.only(left: 20, top: 5, right: 0, bottom: 5),
+              child: SvgPicture.asset('assets/icons/search.svg')),
+          hintText: 'Search...',
+          hintStyle: TextStyle(color: Colors.black87),
+          border: InputBorder.none,
+        ),
+      ),
+      backgroundColor: const Color(0xffB4D8F9),
+      leading: GestureDetector(
+        onTap: () {_navigate(0);},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            color: Color(0xffB4D8F9),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/arrow-left.svg',
+            height: 25,
+            width: 25,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: const BoxDecoration(
+              color: Color(0xffB4D8F9),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/settings.svg',
+              height: 37,
+              width: 37,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
 }
