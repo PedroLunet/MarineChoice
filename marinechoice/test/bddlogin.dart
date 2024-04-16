db572a794338d1cc10c2197a96bdc48703cf176f
jose03pedro <91511511+jose03pedro@users.noreply.github.com>
2024-04-16 16:57:48 +0100

    tests for the map

--- marinechoice/test/bddlogin.dart
+++ marinechoice/test/bddlogin.dart
@@ -1,4 +1,5 @@
 import 'package:bdd_framework/bdd_framework.dart';
+//import 'package:bdd_widget_test/bdd_widget_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:marinechoice/pages/loginpage.dart';
 
@@ -10,5 +11,9 @@ void main() {
       .given('The app is running')
       .when('The user presses the login button.')
       .then('The user gets sent to the homepage')
-      .run((ctx) async {/*???*/});
+      .run((ctx) async {
+    // Clean up or perform any final checks.
+    // For example:
+    //await ctx.world.tearDownApp();
+  });
 }
