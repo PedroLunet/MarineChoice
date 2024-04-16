05e9ebb720db274ddc3e6bb9af12ef144e417797
Pedro Lunet <pedrolunet@gmail.com>
2024-04-16 15:59:57 +0100

    Updated register page

--- marinechoice/lib/pages/registerpage.dart
+++ marinechoice/lib/pages/registerpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import "package:country_picker/country_picker.dart";
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage();
@@ -45,34 +46,84 @@ class RegisterPage extends StatelessWidget {
                 child: TextField(
                   controller: usernameController,
                   decoration: const InputDecoration(
-                    hintText: 'Username',
+                    hintText: 'name',
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 50),
+                child: TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    hintText: 'username',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.symmetric(horizontal: 50),
+                child: DropdownButton<String>(
+                  value: 'age',
+                  onChanged: (String? newValue) {},
+                  items: <String>['age', 'country1', 'country2', 'country3']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.symmetric(horizontal: 50),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showCountryPicker(
+                        context: context,
+                        onSelect: (Country country) {
+                          print('Select country: ${country.displayName}');
+                        },
+                      );
+                    }, child: const Text('country'),
+                  )
+                ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 50),
+                child: TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    hintText: 'e-mail',
+                  ),
+                ),
+              ),
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 50),
                 child: TextField(
                   controller: passwordController,
                   obscureText: true,
                   decoration: const InputDecoration(
-                    hintText: 'Password',
+                    hintText: 'password',
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 50),
                 child: TextField(
                   controller: confirmPasswordController,
                   obscureText: true,
                   decoration: const InputDecoration(
-                    hintText: 'Confirm Password',
+                    hintText: 'confirm password',
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 30),
               ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(const Color(0xff5B92C6)),
+                ),
                 onPressed: () {
                   if (checkPassword()) {
                     Navigator.of(context).push(
