d57bcadcdf2012a3e2020ea1028b4fc30a6e92c5
Cláudia Mesquita <cmmesquita3104@gmail.com>
2024-04-30 22:16:11 +0100

    Added web

--- marinechoice/lib/main.dart
+++ marinechoice/lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    name: 'Marinechoice',
-    options: const FirebaseOptions(
-      apiKey: 'key',
-      appId: 'id',
-      messagingSenderId: 'sendid',
-      projectId: 'myapp',
-      storageBucket: 'myapp-b9yt18.appspot.com',
-  )
-  );
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        name: 'Marinechoice',
+        options: const FirebaseOptions(
+          apiKey: 'key',
+          appId: 'id',
+          messagingSenderId: 'sendid',
+          projectId: 'myapp',
+          storageBucket: 'myapp-b9yt18.appspot.com',
+        ));
+  } else {
+    await Firebase.initializeApp(
+        name: 'Marinechoice',
+        options: const FirebaseOptions(
+          apiKey: 'key',
+          appId: 'id',
+          messagingSenderId: 'sendid',
+          projectId: 'myapp',
+          storageBucket: 'myapp-b9yt18.appspot.com',
+        ));
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -26,8 +39,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
       home: const Wrapper(),
-
     );
   }
 }
-
